604bf759c57836a3e95e697892570ab79c862a40
leetApe <emaasnoel01@gmail.com>
2022-06-20 09:38:26 +0800

    removed unnecessary files

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
+DIR="$HOME/.config/bspwm/polybar"
 
-DIR="$HOME/.config/polybar/shades"
-
-# Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
+# launch the bars
 polybar -q bar-search -c "$DIR"/config.ini &
 polybar -q bar-left -c "$DIR"/config.ini &
 polybar -q bar-center -c "$DIR"/config.ini &
